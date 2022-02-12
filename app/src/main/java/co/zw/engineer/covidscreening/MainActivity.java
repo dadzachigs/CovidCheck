29001237ca0dc4dc998e747e297c36494884eb0f
Darlington Chigumbu <dchigumbu07@gmail.com>
2022-02-12 13:13:15 +0200

    first commit

---
+++ app/src/main/java/co/zw/engineer/covidscreening/MainActivity.java
@@ -0,0 +1,19 @@
+package co.zw.engineer.covidscreening;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+
+
+}
